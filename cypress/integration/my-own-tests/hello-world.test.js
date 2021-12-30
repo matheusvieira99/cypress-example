ee306899e35c06ebf8f68ab10ccc39c13294c946
Matheus Vieira <matheus-vieira12@hotmail.com>
2021-12-30 15:16:43 -0300

    add: checkbox validation

--- cypress/integration/my-own-tests/hello-world.test.js
+++ cypress/integration/my-own-tests/hello-world.test.js
@@ -1,21 +1,34 @@
 ///<reference types="cypress" />
 
 describe('Validar pagina', () => {
-    it('test 1', () => {
-        cy.visit('https://demoqa.com/');
-        
-        //mocha
-        cy.contains('TOOLSQA.COM').should('exist');
+        beforeEach(() => {
+            cy.visit('https://demoqa.com/');
+            
+            //mocha
+            cy.contains('TOOLSQA.COM').should('have.text', '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.');
+    
+            //clicando em elements
+            //usando css selector
+            cy.get('#app > div > div > div.home-body > div > div:nth-child(1) > div > div.card-body').click();
+    
+            //mudar tamanho da tela
+            //cy.viewport('samsung-note9');
+            cy.viewport(1280,720);
 
-        //clicando em elements
-        //1 forma
-        cy.get('#app > div > div > div.home-body > div > div:nth-child(1) > div > div.card-body').click();
+        });
 
-        
+    it('Validar checkbox', () => {
+        cy.get('#item-1').click();
+        cy.get('#tree-node > ol > li > span > button > svg').first().click();
+        cy.get('#tree-node > ol > li > ol > li:nth-child(1) > span > button > svg').click();
+        cy.get('#tree-node > ol > li > ol > li:nth-child(1) > ol > li:nth-child(1) > span > label > span.rct-checkbox > svg').click();
+        cy.get('#tree-node').should('contain.html', 'class="rct-icon rct-icon-check"');
+
+
     })
 
     //teste negativo
-    it('teste negativo', () => {
-        expect(true).to.equal(false);
-    })
+    // it('teste negativo', () => {
+    //     expect(true).to.equal(false);
+    // })
 })
